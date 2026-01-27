226d34eb387b257a888884bfb5ef385959174cdd
slowlydev <me@slowly.dev>
2026-01-27 08:17:43 +0100

    chore: bump dependencies

--- dashboard/eslint.config.mjs
+++ dashboard/eslint.config.mjs
@@ -1,3 +1,6 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import next from "eslint-config-next";
+import nextTypescript from "eslint-config-next/typescript";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
@@ -5,10 +8,10 @@ const compat = new FlatCompat({
 	baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-	...compat.config({
-		extends: ["next", "next/typescript", "prettier"],
-	}),
-];
+const eslintConfig = [...nextCoreWebVitals, ...next, ...nextTypescript, ...compat.config({
+    extends: ["prettier"]
+}), {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
